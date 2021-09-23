485b426b8803a9eedca4d68ea7f5497665f1061c
Loris <logiunta1997@gmail.com>
2021-09-23 10:13:08 +0200

    added scripts to compile and run

---
+++ scripts/windows/compile_client.cmd
@@ -0,0 +1,4 @@
+mkdir bin
+mkdir bin\Worth
+mkdir bin\Worth\Client
+javac -d ./bin/Worth/Client -cp "lib/*" src/Client/*.java src/Common/*.java src/Exceptions/*.java
---
+++ scripts/windows/compile_server.cmd
@@ -0,0 +1,4 @@
+mkdir bin
+mkdir bin\Worth
+mkdir bin\Worth\Server
+javac -d ./bin/Worth/Server -cp "lib/*" src/Server/*.java src/Common/*.java src/Exceptions/*.java
